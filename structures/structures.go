f8efb8ed1a569624efcf49685f01910b8393934b
Milan Kovac <113798238+milan-kovac@users.noreply.github.com>
2024-11-26 21:27:20 +0100

    Maps

--- structures/structures.go
+++ structures/structures.go
@@ -21,4 +21,24 @@ func main() {
 	var sliceC []int = dynamicLength[3:] // [3,4,8] FROM 3 TO LAST
 	fmt.Println(sliceC)
 
+	// definition map where key is string value number
+	scores := map[string]int{
+        "Alice": 90,
+        "Bob":   85,
+        "Charlie": 95,
+    }
+
+	fmt.Println("Scores:", scores)
+
+	// access to the element by key
+	fmt.Println("Alice's score:", scores["Alice"])
+
+	// uses loops with map
+	for key, value := range scores {
+        fmt.Printf("Key: %s, Value: %d\n", key, value)
+    }
+
+	// Delete by key
+	delete(scores, "Bob")
+
 }
